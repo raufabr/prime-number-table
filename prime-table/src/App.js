5bc1048f9190fa6b93bdf5549205af3416087fac
Abrar Rauf <abrarrauf@192.168.1.104>
2021-02-10 18:34:19 +0000

    working solution

--- prime-table/src/App.js
+++ prime-table/src/App.js
@@ -1,43 +1,13 @@
 import React, { useState } from "react";
 import { Table } from "./Table";
 function App() {
-	const [number, setNumber] = useState("");
+	const [input, setInput] = useState("");
+	//const [primeNums, setPrimeNums] = useState([]);
 
 	const handleChange = (event) => {
-		//	console.log("handlechange clicked");
-
-		setNumber(event.target.value);
+		setInput(event.target.value);
 	};
 
-	const isPrime = (num) => {
-		const sqrt = Math.sqrt(num);
-		for (let i = 2; i < sqrt; i++) {
-			if (num % i === 0) {
-				return false;
-			}
-		}
-		return true;
-	};
-
-	const findNPrimes = (nth) => {
-		//start with the first prime number
-		let primes = [2];
-		//start with the count
-		let count = 1;
-		//second prime number
-		let num = 3;
-		//Start from odd number and increase count by 2
-		//if we find prime number then push it to an array and increase count
-		while (count <= nth) {
-			if (isPrime(num)) {
-				primes.push(num);
-				count++;
-			}
-			num += 2;
-		}
-		console.log(primes);
-		return primes;
-	};
 	return (
 		<div>
 			<span>Prime number</span>
@@ -46,11 +16,10 @@ function App() {
 					type="number"
 					name="primeNum"
 					onChange={handleChange}
-					value={number}
+					value={input}
 				/>
 			</span>
-			<input type="submit" value="Submit" />
-			<Table></Table>
+			{input > 2 && <Table input={input} />}
 		</div>
 	);
 }
--- prime-table/src/Table.js
+++ prime-table/src/Table.js
@@ -1,35 +1,60 @@
 import "./Table.css";
-export const Table = (props) => {
+import ReactHtmlParser from "react-html-parser";
+
+export const Table = ({ input }) => {
+	const isPrime = (num) => {
+		const sqrt = Math.sqrt(num);
+		for (let i = 2; i < sqrt; i++) {
+			if (num % i === 0) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+	const findNPrimes = (nth) => {
+		//start with the first prime number
+		let primes = [2];
+		//start with the count
+		let count = 1;
+		//second prime number
+		let num = 3;
+		//Start from odd number and increase count by 2
+		//if we find prime number then push it to an array and increase count
+		while (count <= nth) {
+			if (isPrime(num)) {
+				primes.push(num);
+				count++;
+			}
+			num += 2;
+		}
+		return primes;
+	};
+	const generateTable = (primeNums) => {
+		let output = "";
+		for (let i = 0; i <= primeNums.length - 1; i++) {
+			output += "<tr>" + "<th>" + primeNums[i] + "</th>";
+			for (let j = 0; j <= primeNums.length - 1; j++) {
+				output += "<td>" + primeNums[i] * primeNums[j] + "</td>";
+			}
+			output += "</tr>";
+		}
+		let test = [];
+		console.log(output);
+		return ReactHtmlParser(output);
+	};
+
 	return (
 		<table>
 			<thead>
 				<tr>
-					<th></th>
-					<th>2</th>
-					<th>3</th>
-					<th>5</th>
+					<th />
+					{findNPrimes(input).map((num) => (
+						<th>{num}</th>
+					))}
 				</tr>
 			</thead>
-			<tbody>
-				<tr>
-					<th>2</th>
-					<td>4</td>
-					<td>6</td>
-					<td>10</td>
-				</tr>
-				<tr>
-					<th>3</th>
-					<td>6</td>
-					<td>9</td>
-					<td>15</td>
-				</tr>
-				<tr>
-					<th>5</th>
-					<td>10</td>
-					<td>15</td>
-					<td>25</td>
-				</tr>
-			</tbody>
+			<tbody>{generateTable(findNPrimes(input))}</tbody>
 		</table>
 	);
 };
