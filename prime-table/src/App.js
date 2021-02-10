c7f3a6ef0e018d489024a1f974c31f9caeb91803
Abrar Rauf <abrarrauf@192.168.1.104>
2021-02-10 17:17:14 +0000

    handle user input

--- prime-table/src/App.js
+++ prime-table/src/App.js
@@ -1,5 +1,28 @@
+import React, { useState } from "react";
+
 function App() {
-	return <div>hello</div>;
+	const [number, setNumber] = useState("");
+
+	const handleChange = (event) => {
+		console.log("handlechange clicked");
+
+		setNumber(event.target.value);
+	};
+
+	return (
+		<div>
+			<span>Prime number</span>
+			<span>
+				<input
+					type="text"
+					name="primeNum"
+					onChange={handleChange}
+					value={number}
+				/>
+			</span>
+			<input type="submit" value="Submit" />
+		</div>
+	);
 }
 
 export default App;
