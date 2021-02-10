73b0e80c36bece28e512e5611c79fa7e571184f5
Abrar Rauf <abrarrauf@192.168.1.104>
2021-02-10 17:23:59 +0000

    Check if Isprime works, tested on console

--- prime-table/src/App.js
+++ prime-table/src/App.js
@@ -9,18 +9,31 @@ function App() {
 		setNumber(event.target.value);
 	};
 
+	const isPrime = (num) => {
+		const sqrt = Math.sqrt(num);
+		for (let i = 2; i < sqrt; i++) {
+			if (num % i === 0) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+	const numbers = [2, 3, 5, 6, 7, 8, 9];
+
 	return (
 		<div>
 			<span>Prime number</span>
 			<span>
 				<input
-					type="text"
+					type="number"
 					name="primeNum"
 					onChange={handleChange}
 					value={number}
 				/>
 			</span>
 			<input type="submit" value="Submit" />
+			{console.log(numbers.map((num) => isPrime(num)))}
 		</div>
 	);
 }
